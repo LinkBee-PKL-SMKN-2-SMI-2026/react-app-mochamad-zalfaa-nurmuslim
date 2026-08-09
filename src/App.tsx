8ae1f4c4369eb3194f8398457fa7a91b16a7f5ef
Mochamad Zalfaa Nurmuslim <kodenz726@gmail.com>
2026-08-09 20:28:02 +0700

    fix: Error App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,21 @@ export default function App() {
   ];
 
   return (
-    // Background seluruh halaman (abu-abu muda)
+    
     <div className="min-h-screen bg-gray-100 py-10 px-4">
       
-      {/* Main Container putih di tengah layar */}
+      {
+
+      }
       <div className="max-w-6xl mx-auto bg-white p-8 rounded-2xl shadow-lg border border-gray-200">
         
         <h1 className="text-3xl font-extrabold text-gray-800 mb-8 text-center border-b-2 border-gray-100 pb-4">
           Daftar Karyawan
         </h1>
 
-        {/* Grid Container untuk card */}
+        {
+          
+        }
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {employees.map((emp) => (
             <EmployeeCard 
