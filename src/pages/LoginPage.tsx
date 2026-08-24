05c475e4c3b53f3f107eeda453b2125bf870c5e2
cmarsaaa-collab <cmarsaaa@gmail.com>
2026-08-24 14:45:27 +0700

    feat: addLoginPage funct

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,3 +8,34 @@ interface LoginFormInputs {
   email: string;
   password: string;
 }
+
+export default function LoginPage() {
+  const navigate = useNavigate();
+  const login = useAuthStore((state) => state.login);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormInputs>();
+
+  const onSubmit: SubmitHandler<LoginFormInputs> = async (data) => {
+    try {
+      const response = await api.post("/auth/login", data);
+
+      const token = response.data.data.accessToken;
+
+      login(token);
+
+      alert("Login berhasil!");
+
+      navigate("/vehicles");
+    } catch (error: any) {
+      console.error("Login gagal:", error);
+
+      alert(
+        error.response?.data?.message ||
+          "Email atau password salah"
+      );
+    }
+  };
