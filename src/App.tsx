58d772b04e360086db5bcfe32d754372ba6b9a10
cmarsaaa-collab <cmarsaaa@gmail.com>
2026-08-24 15:18:55 +0700

    fix: salah nempatin route

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import MainLayout from "./components/MainLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
 import VehicleList from "./pages/VehicleList";
 import VehicleAdd from "./pages/VehicleAdd";
-import LoginPage from "./pages/LoginPage"
-import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/vehicles" element={<VehicleList />} />
-        <Route path="/vehicles/new" element={<VehicleAdd />} />
-      </Route>
+      <Route path="/login" element={<LoginPage />} />
 
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-
-        <Route element={<ProtectedRoute />}>
-          <Route element={<MainLayout />}>
-            <Route path="/vehicles" element={<VehicleList />} />
-          </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<MainLayout />}>
+          <Route path="/vehicles" element={<VehicleList />} />
+          <Route path="/vehicles/new" element={<VehicleAdd />} />
         </Route>
-      </Routes>
+      </Route>
 
       <Route
         path="/"
         element={<Navigate to="/vehicles" replace />}
       />
     </Routes>
-
   );
 }
